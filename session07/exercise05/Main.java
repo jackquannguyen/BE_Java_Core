dd4e53fa0789d8a10d159ec420bb619642242701
jackquannguyen <139937406+jackquannguyen@users.noreply.github.com>
2026-08-22 15:35:51 +0200

    Create Main.java

---
+++ session07/exercise05/Main.java
@@ -0,0 +1,11 @@
+package vn.edu.rikkei.session07.ex05;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle1 = new Rectangle(5.5, 10.0);
+        Rectangle rectangle2 = new Rectangle(-2.0, 8.0);
+
+        rectangle1.displayRectangleInfo();
+        rectangle2.displayRectangleInfo();
+    }
+}
