8e72a249ea1b8ebdcea2b20520b7f593a075a725
jackquannguyen <139937406+jackquannguyen@users.noreply.github.com>
2026-08-24 01:40:43 +0200

    Create SystemConfig.java

---
+++ session08/exercise02/SystemConfig.java
@@ -0,0 +1,11 @@
+package vn.edu.rikkei.session08.ex02;
+
+public class SystemConfig {
+
+    public static final String APP_VERSION = "v2.0.5";
+
+    public static final String SYSTEM_NAME =
+            "Rikkei E-Commerce Platform";
+
+    public static final int MAX_REQUESTS = 1000;
+}
