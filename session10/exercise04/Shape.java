36da36ff1934cb4ceb61d3d1e93d4a2433b9275c
jackquannguyen <139937406+jackquannguyen@users.noreply.github.com>
2026-08-31 01:35:32 +0200

    Create Shape.java

---
+++ session10/exercise04/Shape.java
@@ -0,0 +1,12 @@
+package vn.edu.rikkei.session10.ex04;
+
+public class Shape {
+
+    public double getArea() {
+        return 0.0;
+    }
+
+    public String getName() {
+        return "Shape";
+    }
+}
